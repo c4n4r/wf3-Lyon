e7ed1c7634a80572ccb62c699682cf243f734ffb
delphin.hadrien@gmail.com <Carotte188->
2020-11-25 16:04:42 +0100

    début routing vers page article

--- src/Controller/FirstController.php
+++ src/Controller/FirstController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class FirstController extends AbstractController {
@@ -12,13 +13,15 @@ class FirstController extends AbstractController {
             "name" => "Quake 3",
             "support" => "PC",
             "price" => 30,
-            "type" => "FPS"
+            "type" => "FPS",
+            "image" => 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fbogku.com%2Fwp-content%2Fuploads%2F2017%2F07%2FQuake-III-Arena-.jpg&f=1&nofb=1'
         ],
         [
             "name" => "Warcraft 3",
             "support" => "PC",
             "price" => 40,
-            "type" => "RTS"
+            "type" => "RTS",
+            "image" => "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fassets1.ignimgs.com%2F2018%2F11%2F02%2Fwarcraft-3-reforged---scr-8-1541191005572_1280w.jpg&f=1&nofb=1"
         ],
         [
             "name" => "Mario Kart 8",
@@ -30,20 +33,23 @@ class FirstController extends AbstractController {
             "name" => "Halo",
             "support" => "Xbox",
             "price" => 70,
-            "type" => "FPS"
+            "type" => "FPS",
+            "image" => "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fs3.amazonaws.com%2Fdigitaltrends-uploads-prod%2F2017%2F07%2Fhalo-3.jpg&f=1&nofb=1"
         ],
         [
             "name" => "Animal Crossing",
             "support" => "Switch",
             "price" => 60,
-            "type" => "Life Sim"
+            "type" => "Life Sim",
+            "image" => "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic1.srcdn.com%2Fwordpress%2Fwp-content%2Fuploads%2F2020%2F04%2FAnimal-Crossing-New-Horizons-Scariest-Creepy-Villagers.jpg&f=1&nofb=1"
         ]
     ];
 
 
-    public function defaultAction(){
+    public function defaultAction(Request $request){
+        dd($request);
         //faire un tableau qui représente une liste de jeux vidéos.
-        $filter = "PC";
+        $filter = "All";
         //afficher la liste des jeux sur le template avec toutes les informations
 
         //bonus, filtrer par support :
