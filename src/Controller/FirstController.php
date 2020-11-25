83dd488b7ff703360802df627111ddd20c92c4a1
delphin.hadrien@gmail.com <Carotte188->
2020-11-25 11:28:42 +0100

    commit avec les articles

--- src/Controller/FirstController.php
+++ src/Controller/FirstController.php
@@ -10,13 +10,18 @@ class FirstController extends AbstractController {
     public function defaultAction(){
         $siteName = "Mon joli site";
 
-        $users = ["Hadrien", "Pauline", "Jean"];
-
         $articles = [
-            "Lé déconfinement, pas pour tout de suite",
-            "Rugbyman célèbre retrouvé mort...",
-            "Les réfrigirateurs sont moins chers chez but",
-            "Le poulpe était en réalité... Basile Boli"
+            [
+                "title" => "Titre de mon article 1",
+                "description" => "Oh que cet article est intéressant",
+                "content" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dictum urna urna, eget ornare justo auctor at. Phasellus vestibulum nibh ut nunc volutpat auctor. Duis ut convallis odio. Aliquam vitae elementum eros. Nullam et lectus odio. Aliquam ut venenatis arcu. Suspendisse finibus diam augue, in ullamcorper nunc accumsan nec. Vestibulum semper nisi eget erat faucibus dapibus. Quisque vitae gravida mauris. Duis consectetur justo et mauris vulputate interdum. Suspendisse ut sem pretium, ultricies est in, fringilla ipsum.",
+                "author" => "Hadrien Delphin"
+            ],
+            [
+                "title" => "Titre de mon article 2",
+                "description" => "Incroyable, un second article !!!",
+                "author" => "Charlotte"
+            ]
         ];
         return $this->render('default.html.twig',
             ["site"=>$siteName, "articles"=>$articles]);
