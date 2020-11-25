cefa8b39ef98bbc53401bac18d48b0559aae5c5f
delphin.hadrien@gmail.com <Carotte188->
2020-11-25 13:51:16 +0100

    début routing vers page article

--- src/Controller/FirstController.php
+++ src/Controller/FirstController.php
@@ -7,55 +7,62 @@ use Symfony\Component\HttpFoundation\Response;
 
 class FirstController extends AbstractController {
 
+    private $games = [
+        [
+            "name" => "Quake 3",
+            "support" => "PC",
+            "price" => 30,
+            "type" => "FPS"
+        ],
+        [
+            "name" => "Warcraft 3",
+            "support" => "PC",
+            "price" => 40,
+            "type" => "RTS"
+        ],
+        [
+            "name" => "Mario Kart 8",
+            "support" => "Switch",
+            "price" => 50,
+            "type" => "Racing"
+        ],
+        [
+            "name" => "Halo",
+            "support" => "Xbox",
+            "price" => 70,
+            "type" => "FPS"
+        ],
+        [
+            "name" => "Animal Crossing",
+            "support" => "Switch",
+            "price" => 60,
+            "type" => "Life Sim"
+        ]
+    ];
+
+
     public function defaultAction(){
         $siteName = "Mon joli site";
 
         //faire un tableau qui représente une liste de jeux vidéos.
-        $games = [
-            [
-                "name" => "Quake 3",
-                "support" => "PC",
-                "price" => 30,
-                "type" => "FPS"
-            ],
-            [
-                "name" => "Warcraft 3",
-                "support" => "PC",
-                "price" => 40,
-                "type" => "RTS"
-            ],
-            [
-                "name" => "Mario Kart 8",
-                "support" => "Switch",
-                "price" => 50,
-                "type" => "Racing"
-            ],
-            [
-                "name" => "Halo",
-                "support" => "Xbox",
-                "price" => 70,
-                "type" => "FPS"
-            ],
-            [
-                "name" => "Animal Crossing",
-                "support" => "Switch",
-                "price" => 60,
-                "type" => "Life Sim"
-            ]
-        ];
         $filter = "PC";
         //afficher la liste des jeux sur le template avec toutes les informations
 
         //bonus, filtrer par support :
         // envoyer au template une variable filter qui représente le support a filtrer
         return $this->render('default.html.twig',
-            ["site"=>$siteName, "games" => $games, "filter"=>$filter]);
+            ["site"=>$siteName, "games" => $this->games, "filter"=>$filter]);
 
     }
 
-    public function articleAction(){
-
-        return $this->render('article.html.twig');
+    public function articleAction($name){
+        $selectedGame = null;
+        foreach ($this->games as $game){
+            if($game['name'] === $name){
+                $selectedGame = $game;
+            }
+        }
+        return $this->render('article.html.twig', ['game' => $selectedGame]);
 
     }
 
